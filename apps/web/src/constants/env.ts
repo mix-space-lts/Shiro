84856d1bdd88f07cfaf45512cda112601cd18bbb
bcw222 <bcw0613@163.com>
2026-07-12 14:50:07 +0800

    chore: tweaked default env logic

--- apps/web/src/constants/env.ts
+++ apps/web/src/constants/env.ts
@@ -3,12 +3,13 @@ import { env } from 'next-runtime-env'
 import { isClientSide, isDev } from '~/lib/env'
 
 export const API_URL: string = (() => {
-  if (isDev) return env('NEXT_PUBLIC_API_URL') || ''
+  if (isDev) return env('NEXT_PUBLIC_API_URL') || 'http://localhost:2333'
 
   if (isClientSide && env('NEXT_PUBLIC_CLIENT_API_URL')) {
-    return env('NEXT_PUBLIC_CLIENT_API_URL') || ''
+    return env('NEXT_PUBLIC_CLIENT_API_URL') || 'https://example.com/api/v2'
   }
 
-  return env('NEXT_PUBLIC_API_URL') || '/api/v2'
+  return env('NEXT_PUBLIC_API_URL') || 'http://localhost:2333'
 })() as string
-export const GATEWAY_URL = env('NEXT_PUBLIC_GATEWAY_URL') || ''
+export const GATEWAY_URL =
+  env('NEXT_PUBLIC_GATEWAY_URL') || 'http://localhost:2333'
